c64198b6d988c8d1f09290e7c688524841698f84
Albert Vila Calvo <vilacalvo.albert@gmail.com>
2018-09-06 17:40:04 +0200

    Fix book without thumbnail

--- src/Book.js
+++ src/Book.js
@@ -9,7 +9,8 @@ class Book extends Component {
     return (
       <div className="book">
         <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${this.props.book.imageLinks.thumbnail}")` }}></div>
+          <div className="book-cover" style={{ width: 128, height: 193,
+            backgroundImage: `url("${this.props.book.imageLinks ? this.props.book.imageLinks.thumbnail : ''}")` }}></div>
           <div className="book-shelf-changer">
             <select value={this.props.book.shelf} onChange={this.onShelfChange}>
               <option value="move" disabled>Move to...</option>
