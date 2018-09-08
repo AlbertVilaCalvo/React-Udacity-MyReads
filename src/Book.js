6431641b0587ca109fbc1e8240aa2d65366e9b05
Albert Vila Calvo <vilacalvo.albert@gmail.com>
2018-09-08 19:51:28 +0200

    Convert Book to a functional stateless componenet

--- src/Book.js
+++ src/Book.js
@@ -1,31 +1,29 @@
 import React, { Component } from 'react';
 
-class Book extends Component {
-  onShelfChange = (event) => {
-    this.props.onShelfChange({ book: this.props.book, newShelf: event.target.value });
+const Book = (props) => {
+  const onShelfChange = (event) => {
+    props.onShelfChange({ book: props.book, newShelf: event.target.value });
   }
 
-  render() {
-    return (
-      <div className="book">
-        <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193,
-            backgroundImage: `url("${this.props.book.imageLinks ? this.props.book.imageLinks.thumbnail : ''}")` }}></div>
-          <div className="book-shelf-changer">
-            <select value={this.props.book.shelf} onChange={this.onShelfChange}>
-              <option value="move" disabled>Move to...</option>
-              <option value="currentlyReading">Currently Reading</option>
-              <option value="wantToRead">Want to Read</option>
-              <option value="read">Read</option>
-              <option value="none">None</option>
-            </select>
-          </div>
+  return (
+    <div className="book">
+      <div className="book-top">
+        <div className="book-cover" style={{ width: 128, height: 193,
+          backgroundImage: `url("${props.book.imageLinks ? props.book.imageLinks.thumbnail : ''}")` }}></div>
+        <div className="book-shelf-changer">
+          <select value={props.book.shelf} onChange={onShelfChange}>
+            <option value="move" disabled>Move to...</option>
+            <option value="currentlyReading">Currently Reading</option>
+            <option value="wantToRead">Want to Read</option>
+            <option value="read">Read</option>
+            <option value="none">None</option>
+          </select>
         </div>
-        <div className="book-title">{this.props.book.title}</div>
-        <div className="book-authors">{this.props.book.authors ? this.props.book.authors.join(', ') : ''}</div>
       </div>
-    )
-  }
+      <div className="book-title">{props.book.title}</div>
+      <div className="book-authors">{props.book.authors ? props.book.authors.join(', ') : ''}</div>
+    </div>
+  )
 }
 
 export default Book;
