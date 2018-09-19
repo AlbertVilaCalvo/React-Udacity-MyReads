50494cf1f037ca2764c2f7c70d6ceb3cdc9b6b2e
Albert Vila Calvo <vilacalvo.albert@gmail.com>
2018-09-19 12:56:18 +0200

    Add '404 Not Found' with Switch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 import SearchBooks from './SearchBooks';
@@ -39,18 +39,23 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
-        <Route exact path='/' render={() => (
-          <MyBooks
-            books={this.state.books}
-            onShelfChange={this.onMyBooksShelfChange}
-          />
-        )}/>
-        <Route exact path='/search' render={() => (
-          <SearchBooks
-            mybooks={this.state.books}
-            onShelfChange={this.onSearchBooksShelfChange}
-          />
-        )}/>
+        <Switch>
+          <Route exact path='/' render={() => (
+            <MyBooks
+              books={this.state.books}
+              onShelfChange={this.onMyBooksShelfChange}
+            />
+          )}/>
+          <Route exact path='/search' render={() => (
+            <SearchBooks
+              mybooks={this.state.books}
+              onShelfChange={this.onSearchBooksShelfChange}
+            />
+          )}/>
+          <Route render={() => {
+            return <p>404 Not Found</p>
+          }} />
+        </Switch>
       </div>
     )
   }
